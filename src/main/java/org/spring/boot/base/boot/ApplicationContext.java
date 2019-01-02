601615e8ed293a4ce1e7d6e56219961465376e44
wukunmeng <wukunmeng@xinpianchang.com>
2019-01-02 10:52:38 +0800

    添加服务关闭

--- src/main/java/org/spring/boot/base/boot/ApplicationContext.java
+++ src/main/java/org/spring/boot/base/boot/ApplicationContext.java
@@ -15,12 +15,13 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.spring.boot.base.hook.BeforeClosed;
 import org.spring.boot.base.util.Commons;
 import org.spring.boot.base.util.Constants;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -40,6 +41,8 @@ public class ApplicationContext {
 
     private static Logger logger = LoggerFactory.getLogger(ApplicationContext.class);
 
+    private static org.springframework.context.ApplicationContext applicationContext;
+
     public static void main(String[] args) {
         try {
             SpringApplication spring = new SpringApplication(ApplicationContext.class);
@@ -50,17 +53,30 @@ public class ApplicationContext {
                 logger.info("read config:{}", JSONObject.toJSONString(keys, SerializerFeature.PrettyFormat));
                 if(keys == null || keys.length == 0){
                     logger.info("run config:{}", JSONObject.toJSONString(args, SerializerFeature.PrettyFormat));
-                    spring.run(args);
+                    applicationContext = spring.run(args);
                 } else if(keys != null && keys.length > 0){
                     String[] all = new String[args.length + keys.length];
                     System.arraycopy(args,0,all,0,args.length);
                     System.arraycopy(keys,0,all,args.length,keys.length);
                     logger.info("run config:{}", JSONObject.toJSONString(all, SerializerFeature.PrettyFormat));
-                    spring.run(all);
+                    applicationContext = spring.run(all);
                 }
             } else {
-                spring.run(args);
+                applicationContext = spring.run(args);
             }
+            Runtime.getRuntime().addShutdownHook(new Thread(){
+                @Override
+                public void run() {
+                    super.run();
+                    try {
+                        applicationContext.getBean(BeforeClosed.class).beforeClosed();
+                    } catch (NoUniqueBeanDefinitionException e){
+                        //ignore
+                    } catch (NoSuchBeanDefinitionException e){
+                        //ignore
+                    }
+                }
+            });
         } catch (Exception e){
             logger.error("Exception:" + e.getMessage(),e);
         }
